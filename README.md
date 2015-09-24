1a148ebae8198d9823fad54caece9dd4e540493a
junderw <junderwood@bitcoinbank.co.jp>
2015-09-25 02:31:05 +0900

    Finalize

--- README.md
+++ README.md
@@ -1,1 +1,68 @@
 # bitcoin-js-benkyokai
+
+[![NPM](http://img.shields.io/npm/v/bitcoin-js-benkyokai.svg)](https://www.npmjs.org/package/bitcoin-js-benkyokai)
+
+## インストール
+
+`npm install bitcoin-js-benkyokai`
+
+## ブラウザでの動作確認
+
+`npm install -g browserify`
+`browserify -r bitcoin-js-benkyokai -s bitcoin -o bitcoin.js`
+
+フォルダパスに出来上がる「bitcoin.js」を`<script>`タグの中で読み込めば、下記のオブジェクトが使えます。
+
+```javascript
+// オブジェクト
+bitcoin          // bitcoinjs-libのオブジェクト
+bitcoin.Mnemonic // bitcore-mnemonicのニーモニック復元フレーズ
+bitcoin.Buffer   // nodejsのBufferオブジェクト
+bitcoin.Crypto   // crypto-browserifyという、nodejsネイティブのCryptoをbrowserifyに最適化したもの
+
+// 関数
+/**
+ * @param {String} txt - 平文データ
+ * @param {String} passwd - 暗号化用のパスワード
+ * @param {String} [salt] - 暗号化用のソルト
+ * @param {Number} [iterations] - pbkdf2を通す回数
+ * @returns {String} 暗号化されたテキスト
+ */
+bitcoin.encrypt = function (txt, passwd, salt, iterations)
+/**
+ * @param {String} enc - 暗号化データ
+ * @param {String} passwd - 複合化用のパスワード
+ * @param {String} [salt] - 複合化用のソルト
+ * @param {Number} [iterations] - pbkdf2を通す回数
+ * @returns {String} 平文データ
+ */
+bitcoin.decrypt = function (enc, passwd, salt, iterations)
+
+// 使用例
+> var m = bitcoin.Mnemonic(bitcoin.Mnemonic.Words.JAPANESE)
+undefined
+> var HD = m.toBIP44(2)
+undefined
+> var key = HD.getKey(0)
+undefined
+> key.pub.getAddress().toString()
+'1MCSPLE4UGQVwL8sgdyxUrp1cZqE9biHTL'
+> key.toWIF()
+'L2gQNC4jefQkT65mgDWE3ciDtEaDUMiUviYTk3iuWhxJLEW1SrRn'
+> m.phrase
+'ちょさくけん　たたかう　くかん　おいつく　とおす　したうけ　しまる　さんいん　きおち　らくご　さいてき　せんさい'
+> var enc = bitcoin.encrypt(m.phrase, "password")
+undefined
+> enc
+'eeFtfckMiOI5RQLR/l00jn7g6hwEDIBM/AEdjuEWFn6ofEWXpWdepYFAGVlEBVJT/CbaM529cmsqAiWbkt4Y7/SJf+xNfI+sFlJiIDbmMRpOFmL7J582r4TTjpDCDjw0IoTx7nwl6+NxFdMSx3XALjmKCuaHA02tRPt2ofcbvHoqdTU9VtlpIGFUf6HmwwfsaBMlDn2T9Oqufoa92i6uQjmIljeYR8eS0oZV'
+> bitcoin.decrypt(enc, "password")
+'ちょさくけん　たたかう　くかん　おいつく　とおす　したうけ　しまる　さんいん　きおち　らくご　さいてき　せんさい'
+```
+
+## 免責事項
+
+自己責任でお願いします。
+
+## ライセンス
+
+自由に使って下さい。
--- index.js
+++ index.js
@@ -3,25 +3,60 @@
 'use strict';
 
 var Bitcoin = require('bitcoinjs-lib');
-var Mnemonic = require('bitcore-mnemonic');
-var Buffer = require('Buffer');
+Bitcoin.Mnemonic = require('bitcore-mnemonic');
+Bitcoin.HD = require('bitcore').HDPrivateKey
+Bitcoin.Buffer = require('Buffer');
+Bitcoin.Crypto = require('crypto-browserify');
 
-Mnemonic.prototype.toBIP44 = function (account) {
+Bitcoin.encrypt = function (txt, passwd, salt, iterations) {
+  salt = salt || "testsalt";
+  iterations = iterations || 8192;
+  var cipher = Bitcoin.Crypto.createCipher('aes-256-ctr',Bitcoin.Crypto.pbkdf2Sync(passwd, salt, iterations, 32, 'sha256'));
+  var result = cipher.update(txt, 'utf8', 'base64')
+  result += cipher.final('base64')
+  cipher = null;
+  txt = null;
+  passwd = null;
+  salt = null;
+  return result;
+};
+
+Bitcoin.decrypt = function (enc, passwd, salt, iterations) {
+  salt = salt || "testsalt";
+  iterations = iterations || 8192;
+  var decipher = Bitcoin.Crypto.createDecipher('aes-256-ctr',Bitcoin.Crypto.pbkdf2Sync(passwd, salt, iterations, 32, 'sha256'));
+  try {
+    var result = decipher.update(enc, 'base64', 'utf8')
+    result += decipher.final('utf8')
+  } catch(e) {
+    return null;
+  };
+  decipher = null;
+  enc = null;
+  passwd = null;
+  salt = null;
+  return result;
+};
+
+var makeAccount = function (account, pathHead, self) {
   account = parseInt(account) || 0;
   if (typeof account !== "number" || account < 0) return null;
-  return this.toHDPrivateKey().derive("m/44'/0'/" + account + "'");
+  return self.toHDPrivateKey().derive(pathHead + account + "'");
 };
 
-Bitcoin.prototype.derive = function (HD, i, j) {
-  if (!i || typeof i !== "number" || i < 0) return null;
+Bitcoin.Mnemonic.prototype.toBIP32 = function (account) {
+  return makeAccount(account, "m/", this);
+};
+
+Bitcoin.Mnemonic.prototype.toBIP44 = function (account) {
+  return makeAccount(account, "m/44'/0'/", this);
+};
+
+Bitcoin.HD.prototype.getKey = function (i, j) {
+  if (i === null || typeof i !== "number" || i < 0) return null;
   j = parseInt(j) || 0;
   if (typeof j !== "number" || j < 0) return null;
-  return Bitcoin.ECKey.fromWIF(HD.derive("m/" + j + "/" + i).privateKey.toWIF());
+  return Bitcoin.ECKey.fromWIF(this.derive("m/" + j + "/" + i).privateKey.toWIF());
 };
 
-
-module.exports = {
-    Mnemonic: Mnemonic,
-    Bitcoin: Bitcoin,
-    Buffer: Buffer
-};
+module.exports = Bitcoin;
